0a8db191a6a6eaabca41ca625aafed1c74104982
qasimy123 <khawaja@ualberta.ca>
2022-02-23 21:55:04 -0700

    fix: update cmake list and imports after rebase

--- runtime/compiler/optimizer/J9Optimizer.cpp
+++ runtime/compiler/optimizer/J9Optimizer.cpp
@@ -84,6 +84,9 @@
 #include "optimizer/StaticFinalFieldFolding.hpp"
 #include "optimizer/SelectOpt.hpp"
 #include "optimizer/BenefitInliner.hpp"
+#include "optimizer/HandleRecompilationOps.hpp"
+#include "optimizer/MethodHandleTransformer.hpp"
+#include "optimizer/VectorAPIExpansion.hpp"
 
 
 static const OptimizationStrategy J9EarlyGlobalOpts[] =
@@ -808,7 +811,7 @@ J9::Optimizer::Optimizer(TR::Compilation *comp, TR::ResolvedMethodSymbol *method
    // initialize additional J9 optimizations
 
    _opts[OMR::inlining] =
-      new (comp->allocator()) TR::OptimizationManager(self(), TR::SelectOpt<TR_EnableBenefitInliner, TR::BenefitInlinerWrapper, TR_Inliner>::create, OMR::inlining);
+      new (comp->allocator()) TR::OptimizationManager(self(), TR_Inliner::create, OMR::inlining);
    _opts[OMR::targetedInlining] =
       new (comp->allocator()) TR::OptimizationManager(self(), TR_Inliner::create, OMR::targetedInlining);
    _opts[OMR::targetedInlining]->setOptPolicy(new (comp->allocator()) TR_J9JSR292InlinerPolicy(comp));
